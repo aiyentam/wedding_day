e4f733c54c2a396feccaac008e14da1465bb66f1
aiyentam <aiyen1991@gmail.com>
2018-03-04 02:36:28 -0500

    testing materialized css

--- controllers/users.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const hbs = require('hbs')
-const parser = require('body-parser')
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-const passport = require('passport')
-const router = express.Router()
