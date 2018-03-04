7e0a4b4b2a36f7dfa3cd82e0534a615ca22ac4c6
aiyentam <aiyen1991@gmail.com>
2018-03-04 02:09:38 -0500

    adding dependecies

---
+++ controllers/users.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const hbs = require('hbs')
+const parser = require('body-parser')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const router = express.Router()
