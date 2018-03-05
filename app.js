e21cb086de45a532b09ee6a959dd41cc912a0052
aiyentam <aiyen1991@gmail.com>
2018-03-04 20:41:09 -0500

    logo

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ const parser = require('body-parser')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const passport = require('passport')
-const router = express.Router()
+
+const app = express()
+
+const weddingController = require('./controllers/wedding')
+
+app.set("view engine", "hbs")
+app.use(parser.urlencoded({ extended: true }))
+app.use(methodOverride("method"))
+
+app.get("/", (req, res) => {
+  res.render("index")
+})
 
